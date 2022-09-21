564b3d8b66d15a24f8e92106a1daf5f22e4cc66f
Bruno Moyano <108307019+BEMP1@users.noreply.github.com>
2022-09-21 14:01:18 -0300

    Add files via upload

---
+++ tp6ejer1.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+using namespace std;
+
+int eje (double x, double y) {
+	if (x>0) {
+		if (y>0) {
+			return 1;
+		} else {
+			if (y<0) {
+				return 2;
+			} else {
+				return 0;
+			}
+		}
+	}
+	if (x<0) {
+		if (y>0) {
+			return 3;
+		} else {
+			if (y<0) {
+				return 4;
+			} else {
+				return 0;
+			}
+		}
+	}
+	if (x==0) {
+		return 0;
+	}
+	return 0;
+}
+
+int main(int argc, char *argv[]) {
+	double x,y;
+	int resultado;
+	cin>>x;
+	cin>>y;
+	resultado = eje(x,y);
+	cout<<resultado;
+	return 0;
+}
+
